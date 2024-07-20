d3dc52396e8ef0b6c729a97e15d3234026d32729
Shruti jadaun <shrutijadaun@outlook.com>
2024-07-20 13:06:47 +0530

    images added

--- script.js
+++ script.js
@@ -4,42 +4,42 @@ const data = [
         title:'SAINT',
         title2:'ANTONIEN',
         description:'Tucked away in the Switzerland Alps, Saint Antönien offers an idyllic retreat for those seeking tranquility and adventure alike. It\'s a hidden gem for backcountry skiing in winter and boasts lush trails for hiking and mountain biking during the warmer months.',
-        image:'https://assets.codepen.io/3685267/timed-cards-1.jpg'
+        image:'C:\Users\devarsh\Desktop\WebDprojects\timed card opening\timed-cards-1.jpg'
     },
     {
         place:'Japan Alps',
         title:'NANGANO',
         title2:'PREFECTURE',
         description:'Nagano Prefecture, set within the majestic Japan Alps, is a cultural treasure trove with its historic shrines and temples, particularly the famous Zenkō-ji. The region is also a hotspot for skiing and snowboarding, offering some of the country\'s best powder.',
-        image:'https://assets.codepen.io/3685267/timed-cards-2.jpg'
+        image:'C:\Users\devarsh\Desktop\WebDprojects\timed card opening\timed-cards-2.jpg'
     },
     {
         place:'Sahara Desert - Morocco',
         title:'MARRAKECH',
         title2:'MEROUGA',
         description:'The journey from the vibrant souks and palaces of Marrakech to the tranquil, starlit sands of Merzouga showcases the diverse splendor of Morocco. Camel treks and desert camps offer an unforgettable immersion into the nomadic way of life.',
-        image:'https://assets.codepen.io/3685267/timed-cards-3.jpg'
+        image:'C:\Users\devarsh\Desktop\WebDprojects\timed card opening\timed-cards-3.jpg'
     },
     {
         place:'Sierra Nevada - USA',
         title:'YOSEMITE',
         title2:'NATIONAL PARAK',
         description:'Yosemite National Park is a showcase of the American wilderness, revered for its towering granite monoliths, ancient giant sequoias, and thundering waterfalls. The park offers year-round recreational activities, from rock climbing to serene valley walks.',
-        image:'https://assets.codepen.io/3685267/timed-cards-4.jpg'
+        image:'C:\Users\devarsh\Desktop\WebDprojects\timed card opening\timed-cards-4.jpg'
     },
     {
         place:'Tarifa - Spain',
         title:'LOS LANCES',
         title2:'BEACH',
         description:'Los Lances Beach in Tarifa is a coastal paradise known for its consistent winds, making it a world-renowned spot for kitesurfing and windsurfing. The beach\'s long, sandy shores provide ample space for relaxation and sunbathing, with a vibrant atmosphere of beach bars and cafes.',
-        image:'https://assets.codepen.io/3685267/timed-cards-5.jpg'
+        image:'C:\Users\devarsh\Desktop\WebDprojects\timed card opening\timed-cards-5.jpg'
     },
     {
         place:'Cappadocia - Turkey',
         title:'Göreme',
         title2:'Valley',
         description:'Göreme Valley in Cappadocia is a historical marvel set against a unique geological backdrop, where centuries of wind and water have sculpted the landscape into whimsical formations. The valley is also famous for its open-air museums, underground cities, and the enchanting experience of hot air ballooning.',
-        image:'https://assets.codepen.io/3685267/timed-cards-6.jpg'
+        image:'C:\Users\devarsh\Desktop\WebDprojects\timed card opening\timed-cards-6.jpg'
     },
 ]
 
